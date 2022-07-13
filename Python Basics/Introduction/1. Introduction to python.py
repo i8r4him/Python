d5cb1289aceac9b0858b155e86b2cd7e33f53391
“Ibrahim <konflikt.behoben0a@icloud.com>
2022-07-13 21:14:36 +0200

    gherthr

---
+++ Python Basics/Introduction/1. Introduction to python.py
@@ -0,0 +1,50 @@
+# Script Begins
+print("I'm Ibrahim. The best, the strongest")
+# Scripts Ends
+ 
+number = 1 
+print(number)
+
+number2 = 34
+print(number2)
+
+text1 = "Hello or Hallo in german, Wir geht's dir bruder?"
+print(text1)
+
+list1 = []
+
+list1.append(34)
+list1.append("Hello babe")
+list1.append(34.3)
+
+print(list1)
+
+# Python program to illustrate
+# getting input from user
+your_name = input("Enter your name, please: ")
+
+# user entered the name 'irgendwas'
+print("Hello",  your_name)
+print("Hello " + your_name)
+
+
+# accepting integer from the user
+number10 = int(input("Enter onlay a number, please: "))
+number12 = int(input("Enter a nother number to mul. with the first number: "))
+
+# the return type of input() function is string ,
+number99 = number10 * number12
+
+# so we need to convert the input to integer
+print("The product is: ", number99)
+
+# Python program to illustrate
+# selection statement
+nu = int(input("Enter a number, please: "))
+if (nu > 1):
+    print("This number is greater than 1")
+elif(nu < 1):
+    print("This number is smaller than 1")
+else:
+    print("This number is 1")
+
