f870a133f740ed0e0760506b091334c3c722f9c6
“Ibrahim <konflikt.behoben0a@icloud.com>
2022-07-18 18:11:43 +0200

    .

---
+++ Python Basics/Introduction/2. Python 3 basics.py
@@ -0,0 +1,43 @@
+# Python program to illustrate a Dictionary
+
+# creates a empty list
+Dict = []
+
+# putting integer values
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+print(Dict)
+
+# Python program to illustrate a tuple
+	
+# creates a tuple which is immutable
+tup = ('Geeks', 'For', 'Geeks')
+
+print(tup)
+
+#Code submitted by Susobhan AKhuli
+
+# Python program to illustrate a set
+
+# define a set and its elements
+myset = set(["Geeks", "For", "Geeks"])
+
+#as set doesn't have duplicate elements so, 1 geeks will not be printed
+print(myset)
+
+#Code submitted by Susobhan Akhuli
+
+name = input("Enter your name, please: ")
+print("Hello and welcome back", name)
+
+# Python program to illustrate
+# functions
+def hello():
+    name1 = input("Enter your name: ")
+    print("hello again", name1)
+
+# calling function
+hello()
+
+
+
